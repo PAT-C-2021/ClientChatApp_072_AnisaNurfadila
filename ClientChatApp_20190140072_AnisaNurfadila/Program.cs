91503984dce759aadbe3720404547be15e22dbf8
anisanurfadila <anisanfd925@gmail.com>
2022-01-07 23:24:42 +0700

    Add project files.

---
+++ ClientChatApp_20190140072_AnisaNurfadila/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+
+namespace ClientChatApp_20190140072_AnisaNurfadila
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InstanceContext context = new InstanceContext(new ClientCallBack());
+            ServiceReference1.ServiceCallbackClient server = new ServiceReference1.ServiceCallbackClient(context);
+
+            Console.WriteLine("Masukkan Username : ");
+            string nama = Console.ReadLine();
+            server.gabung(nama);
+
+            Console.WriteLine("Kirim Pesan : ");
+            string pesan = Console.ReadLine();
+
+            while (true)
+            {
+                if (!string.IsNullOrEmpty(pesan))
+                    server.kirimPesan(pesan);
+                Console.WriteLine("Kirim Pesan : " + "\n");
+                pesan = Console.ReadLine();
+            }
+        }
+    }
+
+
+
+    public class ClientCallBack : ServiceReference1.IServiceCallbackCallback
+    {
+        public void pesanKirim(string user, string pesan)
+        {
+            Console.WriteLine("{0}: {1}", user, pesan);
+        }
+
+    }
+}
